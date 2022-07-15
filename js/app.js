62714e3ae567a136f46b01304aad7a6b14b23d66
Ngoc Hung <huynhngochung12092001@gmail.com>
2022-07-16 03:50:15 +0700

    Add product page and category page

--- js/app.js
+++ js/app.js
@@ -1,4 +1,30 @@
+// Go to top
+var btnGoToTop = document.querySelector(".go-to-top");
 
+window.onscroll = function () {
+  if (window.scrollY != 0) {
+    btnGoToTop.style = "opacity: 1; pointer-events: unset;";
+  } else {
+    btnGoToTop.style = "opacity: 0; pointer-events: none;";
+  }
+};
 
-
-
+// Slider
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  dots: true,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1024: {
+      items: 1,
+    },
+  },
+});
