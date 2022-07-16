76f87f9e842bf7eed0db1d394d45918d9af37932
Ngoc Hung <huynhngochung12092001@gmail.com>
2022-07-16 23:08:08 +0700

    Add header, footer for PHP

--- admin/partials/header.php
+++ admin/partials/header.php
@@ -1,3 +1,6 @@
+<?php 
+    include('../../config/constants.php')
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,8 +9,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="shortcut icon" href="./assests/images/logo.png" type="image/x-icon" />
-    <link rel="stylesheet" href="./assests/css/carousel/owl.carousel.min.css" />
-    <link rel="stylesheet" href="./assests/css/carousel/owl.theme.default.min.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
@@ -19,26 +20,26 @@
     <div class="header">
         <div class="container header__content">
             <a href="#" class="header__logo">
-                <img class="header__logo-img" src="./assests/images/logo-removebg.png" alt="Logo" />
+                <img class="header__logo-img" src="../../assests/images/logo-removebg.png" alt="Logo" />
             </a>
             <ul class="header__navigation d-flex align-items-center">
                 <li class="header__navigation-item">
                     <a href="#" class="header__navigation-link header__navigation-link--active">Trang chủ</a>
                 </li>
                 <li class="header__navigation-item">
-                    <a href="product.html" class="header__navigation-link">Sản phẩm</a>
+                    <a href="#" class="header__navigation-link">Sản phẩm</a>
                 </li>
                 <li class="header__navigation-item">
-                    <a href="category.html" class="header__navigation-link">Danh mục</a>
+                    <a href="#" class="header__navigation-link">Danh mục</a>
                 </li>
                 <li class="header__navigation-item">
-                    <a href="./contact.html" class="header__navigation-link">Admin</a>
+                    <a href="#" class="header__navigation-link">Admin</a>
                 </li>
             </ul>
-            <div class="header__options d-flex align-items-center">
-                <div class="header__access">
-                    <a href="login.html" class="header__access-link">Đăng nhập</a>
-                    <a href="login.html" class="header__access-link">Đăng xuất</a>
+            <div class="header__options ">
+                <div class="header__access d-flex align-items-center">
+                    <a href="#" class="header__access-link">Đăng nhập</a>
+                    <a href="#" class="header__access-link">Đăng xuất</a>
                 </div>
             </div>
         </div>
---
+++ js/carousel.js
@@ -0,0 +1,19 @@
+// Slider
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  dots: true,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1024: {
+      items: 1,
+    },
+  },
+});
