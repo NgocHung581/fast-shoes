26b9612a2f9379086d7aeb1e3c482e1726622b67
Nguyễn Ngọc Dương <nguyenngocduong220901@gmail.com>
2022-12-02 22:47:00 +0700

    update ui admin page

--- admin/partials/header.php
+++ admin/partials/header.php
@@ -19,6 +19,8 @@ if (!isset($_SESSION['admin'])) {
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
     <link rel="stylesheet" href="../css/style.css" />
+    <!----===== Iconscout CSS ===== -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
@@ -26,7 +28,7 @@ if (!isset($_SESSION['admin'])) {
 </head>
 
 <body>
-    <div class="header">
+    <!-- <div class="header">
         <div class="container header__content">
             <a href="#" class="header__logo">
                 <img class="header__logo-img" src="../../assests/images/logo-removebg.png" alt="Logo" />
@@ -65,17 +67,7 @@ if (!isset($_SESSION['admin'])) {
                         class="header__navigation-link">Đơn hàng</a>
                 </li>
             </ul>
-            <?php
-
-            function echoActiveClassIfRequestMatches($requestUri)
-            {
-                $current_file_name = basename($_SERVER['REQUEST_URI'], ".php");
-
-                if ($current_file_name == $requestUri)
-                    echo 'class="header__active"';
-            }
-
-            ?>
+           
             <div class="header__options ">
                 <div class="header__access d-flex align-items-center">
                     <a href="../../index.php" class="header__access-link">Xem cửa hàng</a>
@@ -90,4 +82,146 @@ if (!isset($_SESSION['admin'])) {
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
+    <nav>
+        <div class="logo-name">
+            <div class="logo-image">
+                <img class="header__logo-img" src="../../assests/images/logo-removebg.png" alt="Logo" />
+
+            </div>
+
+            <span class="logo_name">Fast Shoes</span>
+        </div>
+
+        <div class="menu-items">
+            <ul class="nav-links">
+                <li class="header__navigation-item">
+                    <a href="../manage-home/index.php" class="">
+                        <i class="fa fa-th-large"></i>
+
+                        <span class="link-name">Dahsboard</span>
+                    </a>
+                </li>
+                <!-- <li><a href="#">
+                        <i class="uil uil-estate"></i>
+                        <span class="link-name">Dahsboard</span>
+                    </a></li> -->
+                <li class="header__navigation-item">
+                    <a href="../manage-product/manage-product.php" class=""> <i class="fa fa-box"></i>
+
+                        <span class="link-name">Sản phẩm</span>
+                    </a>
+                </li>
+                <li class="header__navigation-item">
+                    <a href="../manage-category/manage-category.php" class=""> <i class="fa fa-book-open"></i>
+
+                        <span class="link-name">Danh mục</span>
+                    </a>
+                </li>
+                <li class="header__navigation-item">
+                    <a href="../manage-admin/manage-admin.php" class="">
+                        <i class="fa fa-user-tie"></i>
+
+                        <span class="link-name">Admin</span>
+                    </a>
+                </li>
+                <li class="header__navigation-item">
+                    <a href="../manage-customer/manage-customer.php" class=""> <i class="fa fa-user"></i>
+
+                        <span class="link-name">Khách hàng</span>
+                    </a>
+                </li>
+                <li class="header__navigation-item">
+                    <a href="../manage-contact/manage-contact.php" class=""> <i class="fa fa-phone"></i>
+
+                        <span class="link-name">Liên hệ</span>
+                    </a>
+                </li>
+                <li class="header__navigation-item">
+                    <a href="../manage-order/manage-order.php" class="">
+                        <i class="fa fa-list-ul"></i>
+
+                        <span class="link-name">Đơn hàng</span>
+                    </a>
+                </li>
+                <li class="header__navigation-item">
+                    <a href="../../index.php" class="">
+                        <i class="uil uil-estate"></i>
+
+                        <span class="link-name">Cửa hàng</span>
+                    </a>
+                </li>
+                <!-- <li><a href="#">
+                        <i class="uil uil-files-landscapes"></i>
+                        <span class="link-name">Content</span>
+                    </a></li> -->
+                <!-- <li><a href="#">
+                        <i class="uil uil-chart"></i>
+                        <span class="link-name">Analytics</span>
+                    </a></li>
+                <li><a href="#">
+                        <i class="uil uil-thumbs-up"></i>
+                        <span class="link-name">Like</span>
+                    </a></li>
+                <li><a href="#">
+                        <i class="uil uil-comments"></i>
+                        <span class="link-name">Comment</span>
+                    </a></li>
+                <li><a href="#">
+                        <i class="uil uil-share"></i>
+                        <span class="link-name">Share</span>
+                    </a></li> -->
+            </ul>
+
+            <ul class="logout-mode">
+
+
+                <li class="mode">
+
+
+                    <div class="mode-toggle">
+                        <span class="switch"></span>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <section class="dashboard">
+        <div class="top">
+            <i class="uil uil-bars sidebar-toggle"></i>
+
+            <div style="display: flex;justify-content:center;align-items:center">
+                <span class="header__access-link">
+                    <?php
+                        if (isset($_SESSION['admin'])) {
+                            echo $_SESSION['admin'];
+                        }
+                        ?>
+                </span>
+                <a class="log__out-admin" style="display: flex;
+    align-items: center;
+    height: 50px;
+    text-decoration: none;
+    position: relative;
+   " href="../../logout.php">
+                    <i style="" class="uil uil-signout log__out-icon"></i>
+                </a>
+            </div>
+
+
+            <!--<img src="images/profile.jpg" alt="">-->
+        </div>
+        <?php
+
+function echoActiveClassIfRequestMatches($requestUri)
+{
+    $current_file_name = basename($_SERVER['REQUEST_URI'], ".php");
+
+    if ($current_file_name == $requestUri)
+        echo 'class="header__active"';
+}
+
+?>
+
+        <div class="dash-content">
